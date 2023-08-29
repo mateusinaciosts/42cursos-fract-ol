b4db27cc98b2a61c1df661d694d0743ea6cb4d9e
mateus inacio <mateus@pop-os.localdomain>
2023-08-29 12:24:21 -0300

    second commit

---
+++ matsanto/src/init_mlx.c
@@ -0,0 +1,38 @@
+#include "../include/fractol.h"
+
+void	mlx_win_init(t_fractol *f)
+{
+	f->mlx = mlx_init();
+	f->win = mlx_new_window(f->mlx, WIDTH, HEIGTH, "Fractol");
+	f->img = mlx_new_image(f->mlx, WIDTH, HEIGTH);
+	f->img_ptr = mlx_get_data_addr(f->img, &f->bpp, &f->sl, &f->endian);
+}
+
+void	init_fract(t_fractol *f)
+{
+	if (f->fract == 1)
+		mandelbrot_init (f);
+	else if (f->fract == 2)
+		julia_init (f);
+}
+
+int	draw_frac(t_fractol *f)
+{
+	if (f->fract == 1)
+		mandelbrot (f);
+	else if (f->fract == 2)
+		julia (f);
+	mlx_put_image_to_window(f->mlx, f->win, f->img, 0, 0);
+	return (0);
+}
+
+int init_mlx(t_fractol	f)
+{
+    mlx_win_init(&f);
+    init_fract(&f);
+    mlx_loop_hook(f.mlx, draw_frac, &f);
+    mlx_hook(f.win, 17, 0L, close_win, &f);
+    mlx_key_hook(f.win, key_hook, &f);
+    mlx_hook(f.win, 4, 1L << 2, &mouse_zoom, &f);
+    mlx_loop(f.mlx);
+}
---
+++ matsanto/src/main.c
@@ -0,0 +1,52 @@
+
+#include "../include/fractol.h"
+
+void error_message()
+{
+	write(1, "This program renders 2 types of fractal set, ", 46);
+	write(1, "you need to choose one of them.\n", 33);
+	write(1, "\nOptions available: \n", 22);
+	write(1, "\tMandelbrot\n\tJulia parameter1 parameter2\n\n", 43);
+	write(1, "Exemples: \n", 12);
+	write(1, "\t$./fractol mandelbrot\n", 24);
+	write(1, "\t$./fractol julia 0.4 -0.6\n\n", 29);
+	write(1, "Keyboard Shotcuts:\n", 20);
+	write(1, "Q - Iterations Out\t'-' Zoom Out\n", 33);
+	write(1, "E - Iteration In\t'+' Zoom In\n", 30);
+}
+
+int	check_flag(t_fractol *f, int argc, char **argv)
+{
+	if (argc == 2 && ft_strncmp("mandelbrot", argv[1], 11) == 0)
+	{
+		f->fract = 1;
+		return (1);
+	}
+	else if (argc == 4 && ft_strncmp("julia", argv[1], 6) == 0
+		&& ft_atof(argv[2]) >= -2.0 && ft_atof(argv[2]) <= 2.0
+		&& ft_atof(argv[3]) >= -2.0 && ft_atof(argv[3]) <= 2.0
+		&& check_double(argv[2]) && check_double(argv[3]))
+	{
+		f->fract = 2;
+		f->arg_re = ft_atof(argv[2]);
+		f->arg_im = ft_atof(argv[3]);
+		return (1);
+	}
+	else
+		error_message();
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_fractol	f;
+
+	f.x = 0;
+	f.y = 0;
+	if (!check_flag(&f, argc, argv))
+	{
+		exit(0);
+	}
+	init_mlx(f);
+	return (0);
+}
